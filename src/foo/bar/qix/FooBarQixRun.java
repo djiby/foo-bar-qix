6f2ab8be03c669b9ce3cbec06cf4fad6864c7331
Djibril Gueye <dgueye88>
2020-10-05 23:34:15 +0200

    solution v1

--- src/foo/bar/qix/FooBarQixRun.java
+++ src/foo/bar/qix/FooBarQixRun.java
@@ -5,22 +5,25 @@ public class FooBarQixRun {
 	private static final String FOO = "Foo";
 	private static final String BAR = "Bar";
 	private static final String QIX = "Qix";
+	private static final char FOO_3 = '3';
+	private static final char BAR_5 = '5';
+	private static final char QIX_7 = '7';
 
 	public static void main(String[] args) {
 		int nb = 1;
 		while(nb <= 100) {
-			System.out.println(nb + " " + printNumber(nb) );
+			System.out.println(nb + " " + valueNumber(nb) );
 			nb++;
 		}
 
 	}
 	
-	private static String printNumber(int nb) {
-		return divisible(nb).isBlank() && contain(nb).isBlank() 
-				? String.valueOf(nb) : divisible(nb) + contain(nb);
+	private static String valueNumber(int nb) {
+		String str = divisibleNumber(nb) + containNumber(nb);
+		return str.isBlank() ? String.valueOf(nb) : str;
 	}
 	
-	private static String divisible(int nb) {
+	private static String divisibleNumber(int nb) {
 		String str = "";
 		if(nb % 3 == 0) {
 			str = FOO;
@@ -31,18 +34,18 @@ public class FooBarQixRun {
 		return str;
 	}
 	
-	private static String contain(int nb) {
+	private static String containNumber(int nb) {
 		String strNb = String.valueOf(nb);
 		char[] chs = strNb.toCharArray();
 		String str = "";
 		for(char ch : chs) {
-			if(ch == '3') {
+			if(ch == FOO_3) {
 				str = str  + FOO;
 			}
-			if(ch == '5') {
+			if(ch == BAR_5) {
 				str = str + BAR;
 			}
-			if(ch == '7') {
+			if(ch == QIX_7) {
 				str = str + QIX;
 			}
 		}
